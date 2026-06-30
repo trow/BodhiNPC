9ad74a7d37998f26635eafb7dc4f38c1d74d5264
MephistoSatanDevil <MephistoSatanDevil@HotMail.com>
2026-07-01 05:32:56 +0800

    let Elf Bodhi could be summoned

--- BodhiNPC/dialogues/YFFateSpBodhi.d
+++ BodhiNPC/dialogues/YFFateSpBodhi.d
@@ -2,9 +2,17 @@
 // #3 means that Bodhi should be placed after the first 3 lines.
 
 EXTEND_TOP FATESP 6 #3
-+ ~!InMyArea("YFBodhi") Global("YFBodhiSummoned","GLOBAL",0)~ + @0 /*Bring me Bodhi, the bloodthirsty vampire.*/
++ ~!InMyArea("YFBodhi") Global("YFBodhiSummoned","GLOBAL",0) !Global("YFBodhiBecomesElf","GLOBAL",1)~ + @0 /*Bring me Bodhi, that banished elven.*/
 DO ~CreateVisualEffect("SPPORTAL",[1999.1228])
 Wait(2) 
 CreateCreature("YFBod25",[1999.1228],0) 
+SetGlobal("YFBodhiSummoned","GLOBAL",1)~ GOTO 8
+END
+
+EXTEND_TOP FATESP 6 #3
++ ~!InMyArea("YFbodelf") Global("YFBodhiSummoned","GLOBAL",0) Global("YFBodhiBecomesElf","GLOBAL",1)~ + @0 /*Bring me Bodhi, that banished elven.*/
+DO ~CreateVisualEffect("SPPORTAL",[1999.1228])
+Wait(2) 
+CreateCreature("YFbodelf",[1999.1228],0) 
 SetGlobal("YFBodhiSummoned","GLOBAL",1)~ GOTO 8
 END
